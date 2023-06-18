4075f8463b9c34a5eacfdf256ef9c342abca4029
Flumsykun <jgjvanderee@gmail.com>
2023-06-18 23:51:01 +0200

    update 2

--- php/add_student.php
+++ php/add_student.php
@@ -1,8 +1,8 @@
 <?php
 //make connection with database
-$servername = "85942_database";
 $username = "85942";
 $password = "h*a2Pc346";
+$dbname = "85942_database";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
@@ -14,13 +14,23 @@ $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $rollNo = $_POST['rollNo'];
 
-// Insert student data into the database
-$sql = "INSERT INTO students (first_name, last_name, roll_no) VALUES ('$firstName', '$lastName', '$rollNo')";
-if (mysqli_query($conn, $sql)) {
+// Prepare the SQL statement with placeholders
+$sql = "INSERT INTO students (first_name, last_name, roll_no) VALUES (?, ?, ?)";
+
+// Create a prepared statement
+$stmt = mysqli_prepare($conn, $sql);
+
+// Bind the values to the prepared statement
+mysqli_stmt_bind_param($stmt, "sss", $firstName, $lastName, $rollNo);
+
+// Execute the prepared statement
+if (mysqli_stmt_execute($stmt)) {
     echo "Student data added successfully";
 } else {
-    echo "Error adding student data: " . mysqli_error($conn);
+    echo "Error adding student data: " . mysqli_stmt_error($stmt);
 }
 
-mysqli_close($conn);
+// Close the prepared statement
+mysqli_stmt_close($stmt);
+
 ?>
